a88560888fbb14b8015ba7a3e8b528195f4f3cbd
Pratik09W <walalepratik09@gmail.com>
2024-03-28 13:07:30 +0530

    create the routes and done with setup

--- src/app.js
+++ src/app.js
@@ -14,4 +14,9 @@ app.use(express.urlencoded({extended:true,limit :"16kb"}));
 app.use(express.static("publlic"));
 app.use(cookieParser());
 
+
+// routes import
+import router from "./routes/user.routes.js";
+// routes declration
+app.use("/api/v1/users",router);
 export  {app};
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // by using promise and then 
 const asyncHandler = (requestHandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next))
         .catch( (err) => next(err))
     }
